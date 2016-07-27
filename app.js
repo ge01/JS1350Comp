f31f09b8b0ef3bec75448d5b8a0b83b425b51b59
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-27 15:01:41 -0500

    conditional ternary operator

--- app.js
+++ app.js
@@ -3,3 +3,23 @@
 ****************************************************/
 var x = 5;
 document.getElementById("assign5").innerHTML = (x==8);
+
+/****************************************************
+*                 Logical Operators                 *
+****************************************************/
+var x = 6;
+var y = 3;
+
+document.getElementById("logical").innerHTML =
+(x < 10 && y > 1) + "<br>" +
+(x < 10 && y < 1);
+
+/****************************************************
+*                 Ternary Operators                 *
+****************************************************/
+function ternaryFunction() {
+  var age, voteable;
+  age = document.getElementById("age").value;
+  voteable = (age < 18) ? "Too young" : "Old enough";
+  document.getElementById("ternary").innerHTML = voteable + " to vote.";
+}
